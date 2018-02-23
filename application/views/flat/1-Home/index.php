21d6d75371dbe521a2eb987072001d6efc0eb7d0
arjunkashyap <arjun.kashyap@yahoo.co.in>
2018-02-23 13:53:34 +0530

    home page - image included

--- application/views/flat/1-Home/index.php
+++ application/views/flat/1-Home/index.php
@@ -1,3 +1,11 @@
+<div class="container-fluid banner firstDiv">
+    <div class="row justify-content-center overlay">
+        <div class="col-md-12 align-self-center">
+            <h1>Advaitadhārā<br /><small>An Encyclopaedia of Vedantic Works</small></h1>
+        </div>
+        <a class="homepage scrollMore goTo" data-destination="#wordOfTheDay"><span></span>&nbsp;</a>
+    </div>
+</div>
 <div class="container firstDiv word-of-the-day prelims">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -7,12 +15,10 @@
                 <p><em>Seeded by a grant from Sri D P Santhanam Endowment Fund</em></p>
                 <p>Advaita literature is vast, well studied, has continuous stream of contributions and has a well documented grantha paramapara - hierarchy of texts. The aim of the project is to present this immense intellectual treasure in the form of a genealogy tree on the Internet canvass with extensive hyperlinks and details of the authors and their works. A magnificent work by Vidwan R. Thangaswamy, `Bibiliographical Survey Of Advaita Vedanta', published by the University of Madras in 1980 inspires this project which would capture his work in a form designed to facilitate further research in this field. Therefore this project is of immense value to students / researchers of Vedanta and is certain to have a very large number of visits.</p>
             </div>
-            <!-- <p class="see-more"><a href="http://eoh.rkmathbangalore.org/describe/word/Maitreya">read on...</a></p> -->
         </div>
-        <div class="col-md-4">
+        <!-- <div class="col-md-4">
             <img src="<?=PUBLIC_URL?>images/shankara.jpg">
-            <!-- <p class="see-more"><a href="http://eoh.rkmathbangalore.org/describe/word/Maitreya">read on...</a></p> -->
-        </div>
+        </div> -->
     </div>
     <div class="row justify-content-around py-5 my-5">
         <a class="col-md back-blue pill" href="<?=BASE_URL?>Lineage">
