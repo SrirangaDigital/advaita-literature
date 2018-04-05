70d72d27851a5876a457f601f99f280a1457b95e
arjunkashyap <arjun.kashyap@yahoo.co.in>
2018-04-05 15:33:12 +0530

    home page updates

--- application/views/flat/1-Home/index.php
+++ application/views/flat/1-Home/index.php
@@ -6,8 +6,8 @@
         <a class="homepage scrollMore goTo" data-destination="#wordOfTheDay"><span></span>&nbsp;</a>
     </div>
 </div>
-<div class="container-fluid">
-    <div class="row prelims justify-content-center">
+<div class="container-fluid p-0">
+    <div class="row py-5 prelims justify-content-center">
         <div class="col-md-3">
             <img class="img-fluid" src="<?=PUBLIC_URL?>images/shankara.jpg">
         </div>
@@ -19,8 +19,8 @@
             </div>
         </div>
     </div>
-    <div class="row prelims justify-content-center align-items-center">
-        <div class="col-md-8">
+    <div class="row prelims justify-content-center align-items-center" style="color: #FFF; font-weight: 400">
+        <div class="p-5 col-md-8 back-orange" style="color: #FFF;">
             <h4>Indic Academy and Hari Kiran Vadlamani</h4>
             <div class="description">
                <p>Hari Kiran Vadlamani is an entrepreneur, seeker, activist and an aspiring artist. Deeply passionate about art, philosophy, and indology, he has set up, among others, Indic Academy.</p>
@@ -28,7 +28,7 @@
                <p>Over the last decade, he has produced, mentored and acted as a facilitator to several creative and intellectual initiatives by likeminded artists, scholars &amp; public intellectuals.</p>
             </div>
         </div>
-        <div class="col-md-2 text-center">
+        <div class="col-md-4 text-center">
             <img class="img-fluid" src="<?=PUBLIC_URL?>images/indic-academy.jpg"><br />
             <img class="img-fluid mt-3" src="<?=PUBLIC_URL?>images/hkv.jpg">
         </div>
@@ -37,12 +37,12 @@
         <div class="col-md-2 text-center">
             <img class="img-fluid" src="<?=PUBLIC_URL?>images/dps.jpg"><br />
         </div>
-        <div class="col-md-8">
+        <div class="col-md-10">
             <h4>D P Santhanam <small>(1926 - 2012)</small></h4>
             <div class="description">
-                <p>यस्मान्नोद्विजते लोको लोकान्नोद्विजते च यः ।<br />हर्षामर्षभयोद्वेगैर्मुक्तो यः स च मे प्रियः ॥ भगवद्गीता, १२-१५ ॥</p>
+                <p style="color: #fd7f01;">यस्मान्नोद्विजते लोको लोकान्नोद्विजते च यः ।<br />हर्षामर्षभयोद्वेगैर्मुक्तो यः स च मे प्रियः ॥ भगवद्गीता, १२-१५ ॥</p>
                 <p><em>(He for whom no one is put into difficulty and who is not disturbed by anyone, who is equipoised in happiness and distress, fear and anxiety, is very dear to Me.)</em></p>
-                <p>ये तु धर्म्यामृतमिदं यथोक्तं पर्युपासते ।<br /> श्रद्दधाना मत्परमा भक्तास्तेऽतीव मे प्रियाः ॥ भगवद्गीता, १२-२० ॥</p>
+                <p style="color: #fd7f01;">ये तु धर्म्यामृतमिदं यथोक्तं पर्युपासते ।<br /> श्रद्दधाना मत्परमा भक्तास्तेऽतीव मे प्रियाः ॥ भगवद्गीता, १२-२० ॥</p>
                 <p><em>(Those who follow this imperishable path of devotional service and who completely engage themselves with faith, making Me the supreme goal, are very, very dear to Me.)</em></p>
                 <p>D P Santhanam was not a man of renown. He was, in fact, a self-effacing man like several millions of Indians who, with little learning, imbibe the way of Dharma by living immersed in the prescriptions of their Gurus, who most of the time are their parents and grand-parents. </p>
                 <p>Santhanam was one such man. Failing his matriculation, he ran away to join the Royal Air Force as an airman. After the war, over the next six decades he held a series of low to moderately paying jobs. </p>
@@ -59,7 +59,7 @@
         <a class="col-md back-red pill" href="#">
             <p>Advaita<br />Granthavāṅmayam</p>
         </a>
-        <a class="col-md back-orange pill" href="#">
+        <a class="col-md back-blue2 pill" href="#">
             <p>Users &amp;<br />Translations</p>
         </a>
         <a class="col-md back-black pill" href="<?=BASE_URL?>About/Acknowledgement">
