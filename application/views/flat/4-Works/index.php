3a930b78ec3c4d5389a74cfe2f548f119bcc73bb
arjunkashyap <arjun.kashyap@yahoo.co.in>
2018-04-21 13:43:58 +0530

    works landing page in place, placeholders for data included

--- application/views/flat/4-Works/index.php
+++ application/views/flat/4-Works/index.php
@@ -13,74 +13,74 @@
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-blue"></div>
-                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>Works/upanishat/">प्रस्तावना</a></h2></div>
+                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>works/upanishat/">प्रस्तावना</a></h2></div>
                     </div>
                 </article>
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-green"></div>
-                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>Works/upanishat/isha/">ईशावास्योपनिषत्</a></h2></div>
+                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>works/upanishat/isha/">ईशावास्योपनिषत्</a></h2></div>
                     </div>
                 </article>
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-green"></div>
-                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>Works/upanishat/kena/">केनोपनिषत्</a></h2></div>
+                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>works/upanishat/kena/">केनोपनिषत्</a></h2></div>
                     </div>
                 </article>
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-green"></div>
-                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>Works/upanishat/kathaka/">कठोपनिषत्</a></h2></div>
+                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>works/upanishat/kathaka/">कठोपनिषत्</a></h2></div>
                     </div>
                 </article>
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-green"></div>
-                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>Works/upanishat/prashna/">प्रश्नोपनिषत्</a></h2></div>
+                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>works/upanishat/prashna/">प्रश्नोपनिषत्</a></h2></div>
                     </div>
                 </article>
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-green"></div>
-                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>Works/upanishat/mundaka/">मुण्डकोपनिषत्</a></h2></div>
+                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>works/upanishat/mundaka/">मुण्डकोपनिषत्</a></h2></div>
                     </div>
                 </article>
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-green"></div>
-                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>Works/upanishat/mandukya/">माण्डूक्योपनिषत्</a></h2></div>
+                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>works/upanishat/mandukya/">माण्डूक्योपनिषत्</a></h2></div>
                     </div>
                 </article>
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-green"></div>
-                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>Works/upanishat/taittiriya/">तैत्तिरीयोपनिषत्</a></h2></div>
+                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>works/upanishat/taittiriya/">तैत्तिरीयोपनिषत्</a></h2></div>
                     </div>
                 </article>
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-green"></div>
-                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>Works/upanishat/aitareya/">ऐतरेयोपनिषत्</a></h2></div>
+                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>works/upanishat/aitareya/">ऐतरेयोपनिषत्</a></h2></div>
                     </div>
                 </article>
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-green"></div>
-                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>Works/upanishat/chandogya/">छान्दोग्योपनिषत्</a></h2></div>
+                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>works/upanishat/chandogya/">छान्दोग्योपनिषत्</a></h2></div>
                     </div>
                 </article>
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-green"></div>
-                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>Works/upanishat/brihadaranyaka/">बृहदारण्यकोपनिषत्</a></h2></div>
+                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>works/upanishat/brihadaranyaka/">बृहदारण्यकोपनिषत्</a></h2></div>
                     </div>
                 </article>
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-red"></div>
                         <div class="timeline-label">
-                        	<h2><a href="<?=BASE_URL?>Works/upanishat/others/">इतरे</a></h2>
+                        	<h2><a href="<?=BASE_URL?>works/upanishat/others/">इतरे</a></h2>
                     		<p>
 	                    		अथर्वशिखोपनिषत्<br />
 								अथर्वशिर उपनिषदः<br />
@@ -98,20 +98,20 @@
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-blue"></div>
-                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>Works/gita/">प्रस्तावना</a></h2></div>
+                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>works/gita/">प्रस्तावना</a></h2></div>
                     </div>
                 </article>
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-green"></div>
-                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>Works/gita/bhagavadgita/">भगवद्गीता</a></h2></div>
+                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>works/gita/bhagavadgita/">भगवद्गीता</a></h2></div>
                     </div>
                 </article>
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-red"></div>
                         <div class="timeline-label">
-                        	<h2><a href="<?=BASE_URL?>Works/gita/others/">इतरे</a></h2>
+                        	<h2><a href="<?=BASE_URL?>works/gita/others/">इतरे</a></h2>
                     		<p>
 	                    		अष्टावक्रगीता<br />
 	                    		अनुगीता<br />
@@ -129,37 +129,37 @@
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-blue"></div>
-                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>Works/brahmasutra/">प्रस्तावना</a></h2></div>
+                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>works/brahmasutra/">प्रस्तावना</a></h2></div>
                     </div>
                 </article>
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-green"></div>
-                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>Works/brahmasutra/shankara/">शङ्कराचार्यकृतम्-ब्रह्मसूत्रभाष्यम्</a></h2></div>
+                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>works/brahmasutra/shankara/">शङ्कराचार्यकृतम्-ब्रह्मसूत्रभाष्यम्</a></h2></div>
                     </div>
                 </article>
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-green"></div>
-                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>Works/brahmasutra/panchapadika/">पञ्चपादिकाविवरणप्रस्थानम्</a></h2></div>
+                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>works/brahmasutra/panchapadika/">पञ्चपादिकाविवरणप्रस्थानम्</a></h2></div>
                     </div>
                 </article>
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-green"></div>
-                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>Works/brahmasutra/bhamati/">भामतीप्रस्थानम्</a></h2></div>
+                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>works/brahmasutra/bhamati/">भामतीप्रस्थानम्</a></h2></div>
                     </div>
                 </article>
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-green"></div>
-                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>Works/brahmasutra/ratnaprabha/">रत्नप्रभाप्रस्थानम्</a></h2></div>
+                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>works/brahmasutra/ratnaprabha/">रत्नप्रभाप्रस्थानम्</a></h2></div>
                     </div>
                 </article>
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-red"></div>
-                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>Works/brahmasutra/vritti/">ब्रह्मसूत्रवृत्तिग्रन्थाः</a></h2></div>
+                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>works/brahmasutra/vritti/">ब्रह्मसूत्रवृत्तिग्रन्थाः</a></h2></div>
                     </div>
                 </article>
 	        </div>
@@ -170,13 +170,13 @@
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-red"></div>
-                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>Works/prakarana/">प्रकरणग्रन्थाः</a></h2></div>
+                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>works/prakarana/">प्रकरणग्रन्थाः</a></h2></div>
                     </div>
                 </article>
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-red"></div>
-                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>Works/prakarana/vaada/">खण्डनमण्डनपरा वादप्रधानाश्च प्रकरणग्रन्थाः</a></h2></div>
+                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>works/prakarana/vaada/">खण्डनमण्डनपरा वादप्रधानाश्च प्रकरणग्रन्थाः</a></h2></div>
                     </div>
                 </article>
             </div>
@@ -185,13 +185,13 @@
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-red"></div>
-                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>Works/others/reference/">विभिन्नग्रन्थेषु प्रमाणत्वेनोप्न्यस्ताः नाममात्रावशिष्टाश्च ग्रन्थाः</a></h2></div>
+                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>works/others/reference/">विभिन्नग्रन्थेषु प्रमाणत्वेनोप्न्यस्ताः नाममात्रावशिष्टाश्च ग्रन्थाः</a></h2></div>
                     </div>
                 </article>
                 <article class="timeline-entry">
                     <div class="timeline-entry-inner">
                         <div class="timeline-icon back-red"></div>
-                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>Works/others/anonymous/">विभिन्नपुस्तकालयस्थाः मुद्रिता अमुद्रिताश्च अज्ञातकर्तृनामधेया अद्वैतसिद्धान्त प्रधान ग्रन्थाः</a></h2></div>
+                        <div class="timeline-label"><h2><a href="<?=BASE_URL?>works/others/anonymous/">विभिन्नपुस्तकालयस्थाः मुद्रिता अमुद्रिताश्च अज्ञातकर्तृनामधेया अद्वैतसिद्धान्त प्रधान ग्रन्थाः</a></h2></div>
                     </div>
                 </article>
 	        </div>
